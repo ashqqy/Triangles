97772868415d70d9cb0d32211edf4a38abd0f451
Artiom_Fedora <Artiom.shaff07@gmail.com>
2025-09-23 14:22:06 +0300

    Cross and dot product implementation

--- include/triangles.hpp
+++ include/triangles.hpp
@@ -4,7 +4,22 @@ namespace Triangles
 {
 struct Point
 {
+  public:
     Point (float x, float y, float z) : x_(x), y_(y), z_(z) {}
+
+    Point operator+ (const Point& point) const
+    {
+        return Point (x_ + point.x_, y_ + point.y_, z_ + point.z_);
+    }
+
+    Point operator- (const Point& point) const
+    {
+        return Point (x_ - point.x_, y_ - point.y_, z_ - point.z_);
+    }
+
+    float GetX () const { return x_; }
+    float GetY () const { return y_; }
+    float GetZ () const { return z_; }
 
   private:
     const float x_;
@@ -12,10 +27,27 @@ struct Point
     const float z_;
 };
 
+struct Vector
+{
+  public:
+    Vector (const Point vector_end_point) : vector_end_point_ (vector_end_point) {}
+    Vector (const Point& vector_start_point, const Point& vector_end_point) : vector_end_point_ (vector_end_point - vector_start_point) {}
+
+    float GetX () const { return vector_end_point_.GetX (); }
+    float GetY () const { return vector_end_point_.GetY (); }
+    float GetZ () const { return vector_end_point_.GetZ (); }
+
+    static float DotProduct    (const Vector& first, const Vector& second);
+    static Vector CrossProduct (const Vector& first, const Vector& second);
+
+  private:
+    const Point vector_end_point_;
+};
+
 struct Triangle
 {
+  public:
     Triangle (const Point& A, const Point& B, const Point& C) : A_(A), B_(B), C_(C) {}
-
 
   private:
     const Point A_;
--- src/triangles.cpp
+++ src/triangles.cpp
@@ -1,1 +1,18 @@
 #include "triangles.hpp"
+
+namespace Triangles
+{
+inline float Vector::DotProduct (const Vector& first, const Vector& second)
+{
+    return first.GetX() * second.GetX() + first.GetY() * second.GetY() + first.GetZ() * second.GetZ();
+}
+
+inline Vector Vector::CrossProduct (const Vector& first, const Vector& second)
+{
+    float c_x = first.GetY () * second.GetZ () - first.GetZ () * second.GetY ();
+    float c_y = first.GetZ () * second.GetX () - first.GetX () * second.GetZ ();
+    float c_z = first.GetX () * second.GetY () - first.GetY () * second.GetX ();
+
+    return Vector (Point (c_x, c_y, c_z));
+}
+}
